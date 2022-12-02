0fae581aa3220893169f755100d5c0ad82a8b0f4
Tom Denley <t.denley@strategic-blue.com>
2022-12-02 10:54:23 +0000

    tomd: backfill test for day 1

--- src/main/clojure/com/scarytom/aoc_2022/day01.clj
+++ src/main/clojure/com/scarytom/aoc_2022/day01.clj
@@ -2,14 +2,17 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string]))
 
-(defn read-elf-calories []
-  (with-open [reader (io/reader (io/resource "day01_1.txt"))]
-    (->> (line-seq reader)
-         (map #(when-not (string/blank? %)
-                 (Integer/parseInt %)))
-         (partition-by nil?)
-         (take-nth 2)
-         (mapv #(reduce + %)))))
+(defn read-elf-calories
+  ([]
+   (with-open [reader (io/reader (io/resource "day01_1.txt"))]
+     (read-elf-calories (line-seq reader))))
+  ([lines]
+   (->> lines
+        (map #(when-not (string/blank? %)
+                (Integer/parseInt %)))
+        (partition-by nil?)
+        (take-nth 2)
+        (mapv #(reduce + %)))))
 
 (defn part-1 []
   (apply max (read-elf-calories)))
---
+++ src/test/clojure/com/scarytom/aoc_2022/day01_test.clj
@@ -0,0 +1,22 @@
+(ns com.scarytom.aoc-2022.day01-test
+  (:require [clojure.test :refer [deftest is]]
+            [com.scarytom.aoc-2022.day01 :as day01]))
+
+(def sample-lines ["1000"
+                   "2000"
+                   "3000"
+                   ""
+                   "4000"
+                   ""
+                   "5000"
+                   "6000"
+                   ""
+                   "7000"
+                   "8000"
+                   "9000"
+                   ""
+                   "10000"])
+
+(deftest calculates-elf-snack-calories
+  (is (= (day01/read-elf-calories sample-lines)
+         [6000 4000 11000 24000 10000])))
