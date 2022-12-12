b9b385b838ab12636e4721d4537fd43c570b928a
Tom Denley <t.denley@strategic-blue.com>
2022-12-12 00:05:42 +0000

    tomd: something fishy

--- src/main/clojure/com/scarytom/aoc/xmas2022/day09.clj
+++ src/main/clojure/com/scarytom/aoc/xmas2022/day09.clj
@@ -11,70 +11,113 @@
                                        "D" [ 0 -1]))))
 
 (def lookup-table
+  ; relative-head-pos, head-movement -> new-relative-head-pos, tail-movement
   {[[-1  1] [-1  0]] [[-1  0] [-1  1]]
    [[-1  1] [ 1  0]] [[ 0  1] [ 0  0]]
    [[-1  1] [ 0  1]] [[ 0  1] [-1  1]]
    [[-1  1] [ 0 -1]] [[-1  0] [ 0  0]]
+   [[-1  1] [-1  1]] [[-1  1] [-1  1]]
+   [[-1  1] [ 1  1]] [[ 0  1] [ 0  1]]
+   [[-1  1] [ 1 -1]] [[ 0  0] [ 0  0]]
+   [[-1  1] [-1 -1]] [[-1  0] [-1  0]]
 
    [[ 0  1] [-1  0]] [[-1  1] [ 0  0]]
    [[ 0  1] [ 1  0]] [[ 1  1] [ 0  0]]
    [[ 0  1] [ 0  1]] [[ 0  1] [ 0  1]]
    [[ 0  1] [ 0 -1]] [[ 0  0] [ 0  0]]
+   [[ 0  1] [-1  1]] [[ 0  1] [-1  1]]
+   [[ 0  1] [ 1  1]] [[ 0  1] [ 1  1]]
+   [[ 0  1] [ 1 -1]] [[ 1  0] [ 0  0]]
+   [[ 0  1] [-1 -1]] [[-1  0] [ 0  0]]
 
    [[ 1  1] [-1  0]] [[ 0  1] [ 0  0]]
    [[ 1  1] [ 1  0]] [[ 1  0] [ 1  1]]
    [[ 1  1] [ 0  1]] [[ 0  1] [ 1  1]]
    [[ 1  1] [ 0 -1]] [[ 1  0] [ 0  0]]
+   [[ 1  1] [-1  1]] [[ 0  1] [ 0  1]]
+   [[ 1  1] [ 1  1]] [[ 1  1] [ 1  1]]
+   [[ 1  1] [ 1 -1]] [[ 1  0] [ 1  0]]
+   [[ 1  1] [-1 -1]] [[ 0  0] [ 0  0]]
 
    [[ 1  0] [-1  0]] [[ 0  0] [ 0  0]]
    [[ 1  0] [ 1  0]] [[ 1  0] [ 1  0]]
    [[ 1  0] [ 0  1]] [[ 1  1] [ 0  0]]
    [[ 1  0] [ 0 -1]] [[ 1 -1] [ 0  0]]
+   [[ 1  0] [-1  1]] [[ 0  1] [ 0  0]]
+   [[ 1  0] [ 1  1]] [[ 1  0] [ 1  1]]
+   [[ 1  0] [ 1 -1]] [[ 1  0] [ 1 -1]]
+   [[ 1  0] [-1 -1]] [[ 0 -1] [ 0  0]]
 
    [[ 1 -1] [-1  0]] [[ 0 -1] [ 0  0]]
    [[ 1 -1] [ 1  0]] [[ 1  0] [ 1 -1]]
    [[ 1 -1] [ 0  1]] [[ 1  0] [ 0  0]]
    [[ 1 -1] [ 0 -1]] [[ 0 -1] [ 1 -1]]
+   [[ 1 -1] [-1  1]] [[ 0  0] [ 0  0]]
+   [[ 1 -1] [ 1  1]] [[ 1  0] [ 1  0]]
+   [[ 1 -1] [ 1 -1]] [[ 1 -1] [ 1 -1]]
+   [[ 1 -1] [-1 -1]] [[ 0 -1] [ 0 -1]]
 
    [[ 0 -1] [-1  0]] [[-1 -1] [ 0  0]]
    [[ 0 -1] [ 1  0]] [[ 1 -1] [ 0  0]]
    [[ 0 -1] [ 0  1]] [[ 0  0] [ 0  0]]
    [[ 0 -1] [ 0 -1]] [[ 0 -1] [ 0 -1]]
+   [[ 0 -1] [-1  1]] [[-1  0] [ 0  0]]
+   [[ 0 -1] [ 1  1]] [[ 1  0] [ 0  0]]
+   [[ 0 -1] [ 1 -1]] [[ 0 -1] [ 1 -1]]
+   [[ 0 -1] [-1 -1]] [[ 0 -1] [-1 -1]]
 
    [[-1 -1] [-1  0]] [[-1  0] [-1 -1]]
    [[-1 -1] [ 1  0]] [[ 0 -1] [ 0  0]]
    [[-1 -1] [ 0  1]] [[-1  0] [ 0  0]]
    [[-1 -1] [ 0 -1]] [[ 0 -1] [-1 -1]]
+   [[-1 -1] [-1  1]] [[-1  0] [-1  0]]
+   [[-1 -1] [ 1  1]] [[ 0  0] [ 0  0]]
+   [[-1 -1] [ 1 -1]] [[ 0 -1] [ 0 -1]]
+   [[-1 -1] [-1 -1]] [[-1 -1] [-1 -1]]
 
    [[-1  0] [-1  0]] [[-1  0] [-1  0]]
    [[-1  0] [ 1  0]] [[ 0  0] [ 0  0]]
    [[-1  0] [ 0  1]] [[-1  1] [ 0  0]]
-   [[-1  0] [ 0 -1]] [[-1 -1] [ 0  0]]})
+   [[-1  0] [ 0 -1]] [[-1 -1] [ 0  0]]
+   [[-1  0] [-1  1]] [[-1  0] [-1  1]]
+   [[-1  0] [ 1  1]] [[ 0  1] [ 0  0]]
+   [[-1  0] [ 1 -1]] [[ 0 -1] [ 0  0]]
+   [[-1  0] [-1 -1]] [[-1  0] [-1 -1]]})
 
-(defn evaluate-move [relative-head-position move]
-  (if (= relative-head-position [0 0])
-    [move [0 0]]
-    (lookup-table [relative-head-position move])))
+(defn evaluate-single-move [relative-head-position move]
+  (if (= move [0 0])
+    [relative-head-position move]
+    (if (= relative-head-position [0 0])
+      [move [0 0]]
+      (lookup-table [relative-head-position move]))))
 
-(defrecord Tree [height position]
-  Comparable
-  (compareTo [this other]
-    (.compareTo (:height this) (:height other))))
+(defn evaluate-rope-move [relative-knot-positions move]
+  (loop [[relative-knot-position & xs] relative-knot-positions
+         current-move move
+         new-relative-knot-positions nil]
+    (let [[new-relative-knot-pos next-movement] (evaluate-single-move relative-knot-position current-move)]
+      (if (seq xs)
+        (recur xs next-movement (conj new-relative-knot-positions new-relative-knot-pos))
+        [(conj new-relative-knot-positions new-relative-knot-pos) next-movement]))))
 
-(defn count-tail-positions [lines]
+(defn calc-tail-positions [knot-count lines]
   (->> lines
        (map read-instruction)
        (apply concat)
-       (reduce (fn [[[tail-cood :as tail-history] relative-head-position] move]
-                 (let [[new-relative-head-pos tail-movement] (evaluate-move relative-head-position move)]
-                   [(conj tail-history (mapv + tail-cood tail-movement)) new-relative-head-pos]))
-               ['([0 0]) [0 0]])
-       (first)
+       (reduce (fn [[[tail-cood :as tail-history] relative-knot-positions] move]
+                 (let [[new-relative-knot-positions tail-movement] (evaluate-rope-move relative-knot-positions move)]
+                   [(conj tail-history (mapv + tail-cood tail-movement)) new-relative-knot-positions]))
+               ['([0 0]) (repeat (dec knot-count) [0 0])])
+       (first)))
+
+(defn count-tail-positions [knot-count lines]
+  (->> lines
+       (calc-tail-positions knot-count)
        (distinct)
        (count)))
 
 (defn part-1 []
-  (utils/read-input-file 2022 9 count-tail-positions))
+  (utils/read-input-file 2022 9 (partial count-tail-positions 2)))
 
 (defn part-2 []
-  (utils/read-input-file 2022 9 identity))
+  (utils/read-input-file 2022 9 (partial count-tail-positions 10)))
--- src/test/clojure/com/scarytom/aoc/xmas2022/day09_test.clj
+++ src/test/clojure/com/scarytom/aoc/xmas2022/day09_test.clj
@@ -2,15 +2,38 @@
   (:require [clojure.test :refer [deftest is]]
             [com.scarytom.aoc.xmas2022.day09 :as day09]))
 
-(def sample-lines ["R 4"
-                   "U 4"
-                   "L 3"
-                   "D 1"
-                   "R 4"
-                   "D 1"
-                   "L 5"
-                   "R 2"])
+(def sample-lines-1 ["R 4"
+                     "U 4"
+                     "L 3"
+                     "D 1"
+                     "R 4"
+                     "D 1"
+                     "L 5"
+                     "R 2"])
 
+(deftest count-tail-positions-for-two-knot-rope
+  (is (= (day09/count-tail-positions 2 sample-lines-1)
+         13)))
+
+(deftest count-tail-positions-for-ten-knot-rope
+  (is (= (day09/count-tail-positions 10 sample-lines-1)
+         1)))
+
+(def sample-lines-2 ["R 5"
+                     "U 8"
+                     "L 8"
+                     "D 3"
+                     "R 17"
+                     "D 10"
+                     "L 25"
+                     "U 20"])
+
+#_(deftest count-tail-positions-for-ten-knot-rope-mk-2
+  (is (= (distinct (day09/calc-tail-positions 11 sample-lines-2))
+         []))
+
+  (is (= (day09/count-tail-positions 10 sample-lines-2)
+         36)))
 
 (deftest part-1-solution
   (is (= (day09/part-1) 6406)))
