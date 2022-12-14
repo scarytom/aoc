80b2b630e42c93e4da7d80b45a459e88f346d8d5
Tom Denley <t.denley@strategic-blue.com>
2022-12-14 22:05:39 +0000

    tomd: day 14 solution

---
+++ src/main/clojure/com/scarytom/aoc/xmas2022/day14.clj
@@ -0,0 +1,43 @@
+(ns com.scarytom.aoc.xmas2022.day14
+  (:require
+    [clojure.set :as set]
+    [clojure.string :as string]
+    [com.scarytom.aoc.utils :as utils]))
+
+(defn read-filled-coords [line]
+  (->> (string/split line #" -> ")
+       (map #(mapv utils/to-int (string/split % #",")))
+       (partition 2 1)
+       (map (fn [[[x1 y1] [x2 y2]]]
+              (for [x (range (min x1 x2) (inc (max x1 x2)))
+                    y (range (min y1 y2) (inc (max y1 y2)))]
+                [x y])))
+       (apply concat)
+       (set)))
+
+(defn read-structure [lines]
+  (apply set/union (map read-filled-coords lines)))
+
+(defn find-sand-rest-cood [structure origin floor endless-void?]
+  (when-not (structure origin)
+    (loop [[x y :as current-coord] origin]
+      (if (< y floor)
+        (if-let [next-coord (first (remove structure [[x (inc y)] [(dec x) (inc y)] [(inc x) (inc y)]]))]
+          (recur next-coord)
+          current-coord)
+        (when-not endless-void? [x (dec floor)])))))
+
+(defn count-sand [endless-void? lines]
+  (let [initial-structure (read-structure lines)
+        floor (+ 2 (apply max (map second initial-structure)))
+        final-structure (loop [structure initial-structure]
+                          (if-let [coord (find-sand-rest-cood structure [500 0] floor endless-void?)]
+                            (recur (conj structure coord))
+                            structure))]
+    (count (set/difference final-structure initial-structure))))
+
+(defn part-1 []
+  (utils/read-input-file 2022 14 (partial count-sand true)))
+
+(defn part-2 []
+  (utils/read-input-file 2022 14 (partial count-sand false)))
---
+++ src/test/clojure/com/scarytom/aoc/xmas2022/day14_test.clj
@@ -0,0 +1,37 @@
+(ns com.scarytom.aoc.xmas2022.day14-test
+  (:require
+    [clojure.test :refer [deftest is]]
+    [com.scarytom.aoc.xmas2022.day14 :as day14]))
+
+(def sample-lines ["498,4 -> 498,6 -> 496,6"
+                   "503,4 -> 502,4 -> 502,9 -> 494,9"])
+
+(deftest read-filled-coords-test
+  (is (= (day14/read-filled-coords "498,4 -> 498,6 -> 496,6")
+         #{[498 4] [498 5] [498 6] [497 6] [496 6]})))
+
+(deftest reads-structure
+  (is (= (day14/read-structure sample-lines)
+         #{[498 4] [498 5] [498 6]
+           [497 6] [496 6]
+           [503 4] [502 4]
+           [502 5] [502 6] [502 7] [502 8] [502 9]
+           [501 9] [500 9] [499 9] [498 9] [497 9] [496 9] [495 9] [494 9]})))
+
+(deftest finds-next-sand-rest-coord
+  (is (= (day14/find-sand-rest-cood (day14/read-structure sample-lines) [500 0] 9 true)
+         [500 8])))
+
+(deftest count-sand-pt-1
+  (is (= (day14/count-sand true sample-lines)
+         24)))
+
+(deftest count-sand-pt-2
+  (is (= (day14/count-sand false sample-lines)
+         93)))
+
+(deftest part-1-solution
+  (is (= (day14/part-1) 1133)))
+
+(deftest part-2-solution
+  (is (= (day14/part-2) 27566)))
